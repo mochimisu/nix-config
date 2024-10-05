0b34912f39f9d5cd5f90f22d590a04f7b0fd1ca4
mochimisu <brandonwang@me.com>
2024-10-04 22:37:48 -0700

    tweak

--- machines/espresso/home.nix
+++ machines/espresso/home.nix
@@ -6,6 +6,17 @@
         "eDP-1,preferred,0x0,1.6,transform,3"
       ];
     };
+    input = {
+      touchdevice = {
+        transform = 3;
+      };
+    };
+    general = {
+      allow_tearing = "true";
+    };
+    misc = {
+      vfr = "true";
+    };
   };
   imports = [
     ../../home/common-linux.nix
