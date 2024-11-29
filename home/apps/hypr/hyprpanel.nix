7c51568ff51b5bee56f7c1309d73bc2ccce96a4d
mochimisu <brandonwang@me.com>
2024-11-29 14:37:40 -0800

    hyprpanel, walker

--- home/apps/hypr/hyprpanel.nix
+++ home/apps/hypr/hyprpanel.nix
@@ -1,6 +1,6 @@
 { pkgs, ... } : 
 {
-  environment.systemPackages = with pkgs; [
+  home.packages = with pkgs; [
     hyprpanel
   ];
   wayland.windowManager.hyprland = {
@@ -80,5 +80,4 @@
     "tear": true
   }%
   '';
-  };
 }
