20b680b7a831ddacc0db0fddda423d5e508c6f2a
mochimisu <brandonwang@me.com>
2025-07-11 21:37:39 -0700

    fix gnome keyring ssh, up

--- home/apps/zsh/default.nix
+++ home/apps/zsh/default.nix
@@ -1,23 +1,26 @@
-{ pkgs, lib, ... }:
-let thinFastfetch = pkgs.writeShellScriptBin "thin-fastfetch" ''
-#!/bin/bash
-
-# Set the minimum terminal width required to display the logo
-MIN_WIDTH=80
-
-# Get the current terminal width
-TERM_WIDTH=$(tput cols)
-
-# Check if the terminal width meets the minimum requirement
-if [ "$TERM_WIDTH" -ge "$MIN_WIDTH" ]; then
-    # Run Fastfetch with the logo
-    fastfetch
-else
-    # Run Fastfetch without the logo
-    fastfetch --logo none
-fi'';
-in
 {
+  pkgs,
+  lib,
+  ...
+}: let
+  thinFastfetch = pkgs.writeShellScriptBin "thin-fastfetch" ''
+    #!/bin/bash
+
+    # Set the minimum terminal width required to display the logo
+    MIN_WIDTH=80
+
+    # Get the current terminal width
+    TERM_WIDTH=$(tput cols)
+
+    # Check if the terminal width meets the minimum requirement
+    if [ "$TERM_WIDTH" -ge "$MIN_WIDTH" ]; then
+        # Run Fastfetch with the logo
+        fastfetch
+    else
+        # Run Fastfetch without the logo
+        fastfetch --logo none
+    fi'';
+in {
   imports = [
     ./p10k.nix
   ];
@@ -30,20 +33,20 @@ in
     zplug = {
       enable = true;
       plugins = [
-      {
-        name = "zsh-users/zsh-history-substring-search";
-        tags = [ "as:plugin" "depth:1" ];
-      }
-      {
-        name = "chisui/zsh-nix-shell";
-        tags = [ "as:plugin" "depth:1" ];
-      }
+        {
+          name = "zsh-users/zsh-history-substring-search";
+          tags = ["as:plugin" "depth:1"];
+        }
+        {
+          name = "chisui/zsh-nix-shell";
+          tags = ["as:plugin" "depth:1"];
+        }
       ];
     };
     sessionVariables = {
       SDL_VIDEODRIVER = "wayland";
       # for gnome-keyring
-      SSH_AUTH_SOCK = lib.optionalString pkgs.stdenv.isLinux "/run/user/$(id -u)/keyring/ssh";
+      SSH_AUTH_SOCK = lib.optionalString pkgs.stdenv.isLinux "/run/user/$(id -u)/gcr/ssh";
     };
 
     initContent = ''
