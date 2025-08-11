c2c862fc6bb6560c86f62e5032743713aa8c6fba
mochimisu <brandonwang@me.com>
2025-08-11 11:13:31 -0700

    up, uni-sync delay

--- machines/blackmoon/uni-sync.nix
+++ machines/blackmoon/uni-sync.nix
@@ -1,41 +1,41 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   hardware.uni-sync = {
     enable = true;
 
     devices = [
-    {
-      device_id = "VID:3314/PID:41218/SN:6243168001";
-      sync_rgb = true;
-      channels = [
       {
-        mode = "PWM";
-        speed = 100;
+        device_id = "VID:3314/PID:41218/SN:6243168001";
+        sync_rgb = true;
+        channels = [
+          {
+            mode = "PWM";
+            speed = 100;
+          }
+          {
+            mode = "PWM";
+            speed = 100;
+          }
+          {
+            mode = "PWM";
+            speed = 100;
+          }
+          {
+            mode = "PWM";
+            speed = 100;
+          }
+        ];
       }
-      {
-        mode = "PWM";
-        speed = 100;
-      }
-      {
-        mode = "PWM";
-        speed = 100;
-      }
-      {
-        mode = "PWM";
-        speed = 100;
-      }
-      ];
-    }
     ];
   };
 
   systemd.services.uni-sync = {
-    wantedBy = [ "multi-user.target" ];
+    wantedBy = ["multi-user.target"];
     enable = true;
     serviceConfig = {
       User = "root";
       Group = "root";
       ExecStart = "${pkgs.uni-sync}/bin/uni-sync";
+      ExecStartPre = "/bin/sleep 5";
     };
   };
 }
