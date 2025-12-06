055f9db19f940ab546392c3392e0297cc7101b16
mochimisu <brandonwang@me.com>
2025-12-06 00:10:32 -0800

    espresso eww scale and hyprgrass

--- machines/espresso/configuration.nix
+++ machines/espresso/configuration.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, specialArgs, variables, inputs, ... }:
 
 {
+  nixpkgs.overlays = [
+    (import ../../overlays/hyprgrass.nix)
+  ];
+
   networking.hostName = "espresso";
   environment.systemPackages = with pkgs; [
     brightnessctl
