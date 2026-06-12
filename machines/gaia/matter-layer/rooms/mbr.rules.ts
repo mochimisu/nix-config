acbae6cc8456fb910800e82c7833f65e6ff898d9
mochimisu <brandonwang@me.com>
2026-06-11 20:52:02 -0700

    matter, up

--- machines/gaia/matter-layer/rooms/mbr.rules.ts
+++ machines/gaia/matter-layer/rooms/mbr.rules.ts
@@ -1,9 +1,19 @@
-import { defineRoomRules } from "matter-layer/rules";
+import { defineRoomRules, time } from "matter-layer/rules";
 import { bilresaBlinds } from "matter-layer/presets";
 
 export default defineRoomRules("mbr", ({ room, rule }) => {
   bilresaBlinds(room.blindsRemote, room.blinds);
   bilresaBlinds(room.blindsRemote2, room.blinds);
 
-  rule("bed-light", () => room.bedLight.auto(room.presence));
+  rule("bed-light", () => {
+    const activeWindow = time.minuteBetween("8:00", "20:00");
+    room.bedLight.auto(room.presence && activeWindow);
+  });
+  rule("blinds", () => {
+    if (time.minuteBetween("8:00", "20:00")) {
+      room.blinds.open();
+    } else {
+      room.blinds.close();
+    }
+  });
 });
